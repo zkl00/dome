014d978b0bb7cf80a8be48700191a9b0573a2c4f
zkl <1981280278@qq.com>
2020-04-22 22:13:13 +0800

    模态框

---
+++ pages/index/index.js
@@ -0,0 +1,79 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showModal: false, // 显示modal弹窗
+    single: false // false 只显示一个按钮，如果想显示两个改为true即可
+  },
+  btnClick(){
+    this.setData({
+      showModal:true
+    })
+  },
+  modalCancel(e) {
+    // 这里面处理点击取消按钮业务逻辑
+    console.log('点击了取消')
+  },
+  // 点击确定按钮的回调函数
+  modalConfirm(e) {
+   // 这里面处理点击确定按钮业务逻辑
+    console.log('点击了确定')
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
